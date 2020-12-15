7f15de7b7fdc35767c957260ef6893568be139c5
tejas128 <ladtejas128@gmail.com>
2020-12-15 13:28:43 +0530

    Updates

---
+++ precache-manifest.27fddae11ec83d99b8fed5d03e764474.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0723d3c8bccb7b61bbfa22f3e963d6ad",
+    "url": "/sport-site/index.html"
+  },
+  {
+    "revision": "64febf3d16bac859d748",
+    "url": "/sport-site/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "56e1e8972e03dc63aa35",
+    "url": "/sport-site/static/css/main.dd4f7e8c.chunk.css"
+  },
+  {
+    "revision": "64febf3d16bac859d748",
+    "url": "/sport-site/static/js/2.fa1947b7.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/sport-site/static/js/2.fa1947b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e1e8972e03dc63aa35",
+    "url": "/sport-site/static/js/main.ab8dfba4.chunk.js"
+  },
+  {
+    "revision": "386357cb83da6e817f05",
+    "url": "/sport-site/static/js/runtime-main.38f6db47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sport-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
